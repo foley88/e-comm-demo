4906b2ea623b257e29436a596b2cdf1533301b3b
Marco Foley <marcofoley@Marcos-Mac-mini.local>
2023-02-06 20:44:44 +0000

    updated the use of the ID on creation of a customer. Updated the update path, created a new schema for updating a customer.

--- customer-service/database/postgres.py
+++ customer-service/database/postgres.py
@@ -1,45 +1,44 @@
-######################################################################################### 
+#########################################################################################
 # Author: Marco Foley
 # Version: 1.0
-# 
-#  
-# Database - postgres connection file 
+#
+#
+# Database - postgres connection file
 #
 #
 #########################################################################################
 
 
-
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-from sqlalchemy_utils import database_exists, create_database
-
 import logging
 import os
 
-logging.basicConfig(filename='customer_service.log', 
-format='%(asctime)s - %(levelname)s - %(filename)s - %(message)s', 
-level=logging.DEBUG, force= True)
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, database_exists
+
+logging.basicConfig(
+    filename="customer_service.log",
+    format="%(asctime)s - %(levelname)s - %(filename)s - %(message)s",
+    level=logging.DEBUG,
+    force=True,
+)
 logger = logging.getLogger(__name__)
 load_dotenv()
-
-
 
 
 url = f"postgresql://{os.environ['USERNAME']}:{os.environ['PASSWORD']}@{os.environ['HOST']}:{os.environ['PORT']}/{os.environ['DATABASE']}"
 
 
 engine = create_engine(url, echo=True)
-   
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)   
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 Base = declarative_base()
-    
- 
+
 
 def get_db():
     db = SessionLocal()
@@ -47,7 +46,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-
-    
--- customer-service/src/schemas.py
+++ customer-service/src/schemas.py
@@ -1,47 +1,51 @@
 #########################################################################################################
-# 
+#
 # Author: Marco Foley
 # Version: 1.0
-# 
-#  
-# API Schemas 
+#
+#
+# API Schemas
 #
 #
 ########################################################################################################
 
 
-
+from datetime import date, datetime
+from uuid import UUID, uuid4
 
 from pydantic import BaseModel, EmailStr, ValidationError, validator
-from datetime import datetime
-from datetime import date
-from uuid import uuid4, UUID
 from sqlmodel import Field
+
 
 class Address(BaseModel):
     address_line_1: str
     address_line_2: str
     address_line_3: str = None
-    country: str 
-    
+    country: str
 
 
 class Customer(BaseModel):
-    id : UUID = Field(default_factory=uuid4)
     firstname: str
     lastname: str
     email: EmailStr
     address: Address
     password: str
-    
-    
+
     class Config:
         orm_mode = True
 
+
 class CustomerResponse(BaseModel):
     email: EmailStr = None
-  
+
     class Config:
         orm_mode = True
-    
-   
+
+
+class Update_Customer(BaseModel):
+    firstname: str
+    lastname: str
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
